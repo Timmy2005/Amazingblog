017c4aa6437c412888985155bdef1beea54530c9
Timothy Cleveland <tjcp2@k12albemarle.org>
2016-11-28 07:47:12 -0500

    Init...

--- app/__init__.py
+++ app/__init__.py
@@ -4,10 +4,13 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
 from flask.ext.openid import OpenID
 from config import basedir
+from flask.ext.heroku import Heroku
+
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
+heroku = Heroku(app)
 lm = LoginManager()
 lm.init_app(app)
 lm.login_view = 'login'
