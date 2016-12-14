75316c736beb07ece4ec4414cb7c5b482f83c527
Timothy Cleveland <tjcp2@k12albemarle.org>
2016-12-13 20:44:38 -0500

    stuff

--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
 from app import db
+from app import app
+import flask.ext.whooshalchemy as whooshalchemy
 
 
 class User(db.Model):
@@ -31,6 +33,7 @@ class User(db.Model):
 
 
 class Post(db.Model):
+    __searchable__ = ['body']
 
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String())
@@ -42,6 +45,8 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post %r>' % self.title
+
+whooshalchemy.whoosh_index(app, Post)
 
 
 class Youtube(db.Model):
