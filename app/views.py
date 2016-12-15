7004a53a574829ae0d3a644685a113f520cfb571
Timothy Cleveland <tjcp2@k12albemarle.org>
2016-12-14 19:51:48 -0500

    stuff

--- app/views.py
+++ app/views.py
@@ -293,7 +293,7 @@ def delete():
 def search(query, page=1):
     if request.cookies.get('user'):
         current_user = request.cookies.get('user')
-        posts = Post.query.filter(Post.title.contains(query)).order_by(Post.id.desc())
+        posts = Post.query.filter(Post.title.contains(query)).order_by(Post.id.desc()).paginate(page, POSTS_PER_PAGE, False)
         users = User.query.all()
         return render_template('search_results.html',
                                title='Search Results',
